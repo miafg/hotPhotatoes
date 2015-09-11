9a89dd1b7d450db40a2208e771b78a4ba269171c
miafg <miaf@sas.upenn.edu>
2015-09-11 10:22:09 -0400

    first commit

---
+++ HotPhotatoes/FriendViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FriendViewController.swift
+//  HotPhotatoes
+//
+//  Created by Mia Froehling-Gallier on 9/8/15.
+//  Copyright (c) 2015 hotPhotatoes. All rights reserved.
+//
+
+import Cocoa
+
+class FriendViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ HotPhotatoes/SignUpController.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpController.swift
+//  HotPhotatoes
+//
+//  Created by Mia Froehling-Gallier on 9/8/15.
+//  Copyright (c) 2015 hotPhotatoes. All rights reserved.
+//
+
+import Foundation
